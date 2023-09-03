ab8f884224cbbe932f6a529d84d4fb6308f5581e
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-03 10:45:42 +0900

    [Edit]Hello file

--- Desktop/JavaMasterClass/HelloWorld/src/Hello.java
+++ Desktop/JavaMasterClass/HelloWorld/src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Hello, Tim ");
+        System.out.println("Hello, Tim");
         System.out.print("Hello World");
     }
 }
