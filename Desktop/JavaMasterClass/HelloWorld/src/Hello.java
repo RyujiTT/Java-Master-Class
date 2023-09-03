d54f44938010ae96566f23e2ed8ca0dcea8084be
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-03 10:56:13 +0900

    [Done]First_if-then lecture

--- Desktop/JavaMasterClass/HelloWorld/src/Hello.java
+++ Desktop/JavaMasterClass/HelloWorld/src/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Tim");
-        System.out.print("Hello World");
+
+        boolean isAilen = false;
+        if (isAilen == true) {
+            System.out.println("It is not an alien!");
+        }else {
+            System.out.println("It is an alien");
+        }
     }
 }
