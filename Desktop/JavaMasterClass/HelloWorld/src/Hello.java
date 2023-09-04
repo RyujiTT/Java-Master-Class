eb3bac799ba150ff49bce430415561f5beef44f8
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-04 18:55:31 +0900

    [Finish]IntelliJ Basics

--- Desktop/JavaMasterClass/HelloWorld/src/Hello.java
+++ Desktop/JavaMasterClass/HelloWorld/src/Hello.java
@@ -41,5 +41,27 @@ public class Hello {
 
         String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
         System.out.println(s);
+
+        /* Challenge
+        Step1: create a double variable with a value of 20.00.
+        Step2: create a second variable of type double with a value 80.00.
+        Step3: add both numbers together, then multiply by 100.00.
+        Step4: use the remainder operator, to figure out what the remainder from th result of the operation in step three, and 40.00, will be.
+        Step5: create a boolean variable that assigns the value true, if the remainder in step four is 0.00, or false if it's not zero.
+        Step6: output the boolean variable just to see what the result is.
+        Step7: write an if-then statement that displays a message, 'got some remainder'. if the boolean in step five is not true
+        */
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myTotalValue = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyTotalValue =" + myTotalValue);
+        double theRemainder = myTotalValue % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = theRemainder == 0 ? true : false;
+        System.out.println(isNoRemainder);
+        if (!isNoRemainder){
+            System.out.println("I got some remainder");
+        }
     }
 }
