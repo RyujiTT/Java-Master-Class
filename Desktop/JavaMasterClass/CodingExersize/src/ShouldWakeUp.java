9bcbde65c62e05be7810fc3a25b87a1f9969a1c0
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-09 11:31:47 +0900

    [Done]ShouldWakeUp

--- Desktop/JavaMasterClass/CodingExersize/src/ShouldWakeUp.java
+++ Desktop/JavaMasterClass/CodingExersize/src/ShouldWakeUp.java
@@ -1,5 +1,12 @@
 public class ShouldWakeUp {
     public static void main(String[] args) {
+        shouldWakeUp(false, 5);
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        return barking && hourOfDay>=0 && (hourOfDay<8 || hourOfDay==23);
 
     }
 
