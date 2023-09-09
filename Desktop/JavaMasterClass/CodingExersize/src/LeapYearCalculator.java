e8b17c2787381793a61901df5dd7231dcb8b5f43
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-09 14:50:42 +0900

    [Done]IsLeapYear

--- Desktop/JavaMasterClass/CodingExersize/src/LeapYearCalculator.java
+++ Desktop/JavaMasterClass/CodingExersize/src/LeapYearCalculator.java
@@ -1,6 +1,16 @@
 public class LeapYearCalculator {
     public static void main(String[] args) {
+        boolean leapYear= isLeapYear(2012);
 
+        if (leapYear){
+            System.out.println("This year is leap!");
+        } else {
+            System.out.println("This year is invalid or not leap year.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year >= 1 && year <= 9999) && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
     }
 
 }
