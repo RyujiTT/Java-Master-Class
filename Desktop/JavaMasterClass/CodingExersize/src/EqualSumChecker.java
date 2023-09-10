0b5ef1529a9da2aabbc016fe69e2778ac0dc37f0
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-10 19:16:36 +0900

    [Done]EqualSumChecker

--- Desktop/JavaMasterClass/CodingExersize/src/EqualSumChecker.java
+++ Desktop/JavaMasterClass/CodingExersize/src/EqualSumChecker.java
@@ -1,6 +1,15 @@
 public class EqualSumChecker {
     public static void main(String[] args) {
+        boolean foo = hasEqualSum(2,3,5);
+        if(foo){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+
 
     }
-
+    public static boolean hasEqualSum(int num1, int num2, int num3){
+        return num1 + num2 == num3;
+    }
 }
