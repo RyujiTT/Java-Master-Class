88299def5e339b699e3f18da86d62e29552ee492
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-10 19:30:13 +0900

    [Done]TeenNumberChecker

--- Desktop/JavaMasterClass/CodingExersize/src/TeenNumberChecker.java
+++ Desktop/JavaMasterClass/CodingExersize/src/TeenNumberChecker.java
@@ -1,5 +1,18 @@
 public class TeenNumberChecker {
     public static void main(String[] args) {
+        boolean foo = hasTeen(53,41,11);
+        if(foo){
+            System.out.println("There is teenager!");
+        }else{
+            System.out.println("There is no teenager");
+        }
 
+    }
+    public static boolean hasTeen(int age1, int age2, int age3){
+        return (age1 >= 13 && age1 <= 19) || (age2 >= 13 && age2 <= 19) || (age3 >= 13 && age3 <= 19);
+    }
+
+    public static boolean isTeen(int age1){
+        return (age1 >= 13 && age1 <= 19);
     }
 }
