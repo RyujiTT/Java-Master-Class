f43c06a5b4d4ef081396c8e3eb03ce984535a0e0
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-10 22:43:53 +0900

    [Done]OverloadedChellenge

--- Desktop/JavaMasterClass/OverloadedChallenge/src/Main.java
+++ Desktop/JavaMasterClass/OverloadedChallenge/src/Main.java
@@ -2,7 +2,19 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("5ft, 8in = " + convertToCentimeters(5,8) + " cm");
+        System.out.println("68 = " + convertToCentimeters(68) + " cm");
     }
 
+    public static double convertToCentimeters(int inches){
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches){
+//        return convertToCentimeters((feet * 12) + inches);
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+        double result = convertToCentimeters((totalInches));
+        return result;
+    }
 }
