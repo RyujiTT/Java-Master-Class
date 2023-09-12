bafabf30fbcffa1fc57bcba423b95a1f462155db
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-12 10:03:36 +0900

    [Done]PlayingCat

--- Desktop/JavaMasterClass/CodingExercises/src/PlayingCat.java
+++ Desktop/JavaMasterClass/CodingExercises/src/PlayingCat.java
@@ -1,2 +1,17 @@
 public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 45));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+//        if(summer && (temperature >= 25 && temperature <= 45)){
+//            return true;
+//        } else if(!(summer) && (temperature >= 25 && temperature <= 35)){
+//            return true;
+//        }
+//        return false;
+        // refactored code.
+        return temperature >= 25 && temperature <= (summer ? 45 : 35);
+    }
 }
