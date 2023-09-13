717b3813e05f3e52fb830659352536b1933f0fca
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-13 13:43:16 +0900

    [Add]New Project Switch

---
+++ Desktop/JavaMasterClass/Switch/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int value = 4;
+        if (value == 1){
+            System.out.println("Value was 1");
+        } else if (value == 2){
+            System.out.println("Value was 2");
+        }else {
+            System.out.println("Was not 1 or 2");
+        }
+    }
+}
