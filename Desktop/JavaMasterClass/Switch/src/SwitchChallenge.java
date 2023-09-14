2bacee4a90954f7f00df0ea0e257980ff3b871bb
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-14 16:23:47 +0900

    [Done]Traditional Switch Statement

--- Desktop/JavaMasterClass/Switch/src/SwitchChallenge.java
+++ Desktop/JavaMasterClass/Switch/src/SwitchChallenge.java
@@ -1,5 +1,27 @@
 public class SwitchChallenge {
     public static void main(String[] args) {
 
+        char charValue = 'g';
+        switch (charValue){
+            case 'A':
+                System.out.println("A is able");
+                break;
+            case 'B':
+                System.out.println("B is baker");
+                break;
+            case 'C':
+                System.out.println("C is charlie");
+                break;
+            case 'D':
+                System.out.println("D is dog");
+                break;
+            case 'E':
+                System.out.println("E is Easy");
+                break;
+            default:
+                System.out.println("Letter " + charValue + " was not found in the switch");
+                break;
+        }
+
     }
 }
