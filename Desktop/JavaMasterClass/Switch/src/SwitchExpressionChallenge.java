d507dd39d77abc20a371ee8a75f171f2fd70d4d5
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-14 16:42:22 +0900

    [Done]Switch Expression Challenge

--- Desktop/JavaMasterClass/Switch/src/SwitchExpressionChallenge.java
+++ Desktop/JavaMasterClass/Switch/src/SwitchExpressionChallenge.java
@@ -1,9 +1,45 @@
 public class SwitchExpressionChallenge {
     public static void main(String[] args) {
+        printDayOfWeek(5);
+        printWeekDay(3);
 
     }
 
     public static void printDayOfWeek(int day) {
 
+        String dayOfWeek = switch (day){
+            case 0 -> {yield "Sunday"; }
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+
+        System.out.println(day + " stands for " + dayOfWeek);
+    }
+
+    public static void printWeekDay(int day){
+
+        String dayOfWeek = "Invalid Day";
+
+        if(day == 0){
+            dayOfWeek = "Sunday";
+        }else if(day == 1){
+            dayOfWeek = "Monday";
+        }else if(day == 2){
+            dayOfWeek = "Tuesday";
+        }else if(day == 3){
+            dayOfWeek = "Wednesday";
+        }else if(day == 4){
+            dayOfWeek = "Thursday";
+        }else if(day == 5){
+            dayOfWeek = "Friday";
+        }else if(day == 6){
+            dayOfWeek = "Saturday";
+        }
+        System.out.println(day + " stands for " + dayOfWeek);
     }
 }
