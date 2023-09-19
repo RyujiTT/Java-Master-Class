95627ded679ae68ef9a711562c409ba67e72455e
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-19 18:57:38 +0900

    [Done]Sum Odd Range Challenge

--- Desktop/JavaMasterClass/CodingExercises/src/SumOddRange.java
+++ Desktop/JavaMasterClass/CodingExercises/src/SumOddRange.java
@@ -1,5 +1,26 @@
 public class SumOddRange {
     public static void main(String[] args) {
+        System.out.println(isOdd(9));
+        System.out.println(sumOdd(1, 45));
+    }
+
+    public static boolean isOdd(int number) {
+        return (number % 2) > 0;
+    }
+
+    public static int sumOdd(int start, int end) {
+
+        int sum = 0;
+
+        if (start < 1 || end < start) {
+            return -1;
+        }
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+        return sum;
 
     }
 }
