5d197315e6c137f1fb7a24f1bba0a9886eeaa69c
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-19 18:42:04 +0900

    [Done]Sum 3 and 5 challenge

--- Desktop/JavaMasterClass/Sum3And5Challenge/src/Main.java
+++ Desktop/JavaMasterClass/Sum3And5Challenge/src/Main.java
@@ -2,6 +2,21 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int countOfMatches = 0;
+        int sumOfMatches = 0;
 
+        for (int loopNumber = 1; loopNumber <= 1000; loopNumber++){
+            if ((loopNumber % 3 == 0) && (loopNumber % 5 == 0)){
+                countOfMatches++;
+                sumOfMatches += loopNumber;
+                System.out.println("Found a match = " + loopNumber);
+            }
+
+            if (countOfMatches == 5){
+                break;
+            }
+        }
+
+        System.out.println("Sum = " + sumOfMatches);
     }
 }
