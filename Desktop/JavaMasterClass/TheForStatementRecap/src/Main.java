2fb17fd93a66ee0621046287f28651532c73bb84
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-19 18:49:38 +0900

    [Done]For Statement Reccap

--- Desktop/JavaMasterClass/TheForStatementRecap/src/Main.java
+++ Desktop/JavaMasterClass/TheForStatementRecap/src/Main.java
@@ -3,5 +3,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 100; i < 130; i += 10) {
+            System.out.println("Number = " + i);
+        }
+
     }
 }
