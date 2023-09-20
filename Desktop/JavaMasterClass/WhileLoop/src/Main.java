405ec3cdf5bd30b61a6194bd5fa524b7f90d6415
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-20 13:52:27 +0900

    [Done]While Loop

--- Desktop/JavaMasterClass/WhileLoop/src/Main.java
+++ Desktop/JavaMasterClass/WhileLoop/src/Main.java
@@ -2,16 +2,29 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        for (int i = 1; i <= 5; i++){
+//            System.out.println(i);
+//        }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+//        int j = 1;
+//        boolean isReady = false;
+//        do {
+//            if (j > 5){
+//                break;
+//            }
+//            System.out.println(j);
+//            j++;
+//            isReady = (j > 0);
+//        } while (isReady);
+
+        int number = 0;
+        while (number < 50){
+            number += 5;
+            if (number % 25 == 0){
+                continue;
+            }
+            System.out.println(number + "_");
         }
     }
 }
