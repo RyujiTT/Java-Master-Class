562ae260c6e1057372cdd50aa56df6c29a8bb846
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-20 16:30:38 +0900

    [Done]The While Loop Challenge Step1

--- Desktop/JavaMasterClass/WhileLoop/src/isEvenNumber.java
+++ Desktop/JavaMasterClass/WhileLoop/src/isEvenNumber.java
@@ -1,2 +1,19 @@
 public class isEvenNumber {
+    public static void main(String[] args) {
+
+        int number = 4;
+        int finishNumber = 20;
+
+        while (number <= finishNumber) {
+            number++;
+            if (!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("Even number " + number);
+        }
+    }
+
+    public static boolean isEvenNumber(int number){
+        return (number % 2 == 0);
+    }
 }
