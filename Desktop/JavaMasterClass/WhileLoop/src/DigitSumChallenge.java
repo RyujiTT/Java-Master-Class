b69976a1c981254569220d1c81c9a983d931d1cd
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-20 16:42:54 +0900

    [Done]Digits Sum Challenge

--- Desktop/JavaMasterClass/WhileLoop/src/DigitSumChallenge.java
+++ Desktop/JavaMasterClass/WhileLoop/src/DigitSumChallenge.java
@@ -1,8 +1,22 @@
 public class DigitSumChallenge {
     public static void main(String[] args) {
 
+        System.out.println("sumDigits in number 1234 is " + sumDigits(1234));
+
     }
     public static int sumDigits(int number){
+        if (number < 0){
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 9){
+            sum += (number % 10);
+            number = number / 10;
+        }
+        sum += number;
+        return sum;
 
     }
 }
