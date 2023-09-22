12b65123537bba19f3d6f8d5a154e1738678df67
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-22 13:36:28 +0900

    [Done]First and Last Digit Sum

--- Desktop/JavaMasterClass/CodingExercises/src/FirstAndLastDigitSum.java
+++ Desktop/JavaMasterClass/CodingExercises/src/FirstAndLastDigitSum.java
@@ -1,6 +1,16 @@
 public class FirstAndLastDigitSum {
     public static void main(String[] args) {
-
+        System.out.println(sumFirstAndLastDigit(13542));
     }
 
+    public static int sumFirstAndLastDigit(int number){
+        if (number < 0){
+            return -1;
+        }
+        int lastNumber = number % 10;
+        while(number > 9){
+            number /= 10;
+        }
+        return lastNumber + number;
+    }
 }
