dadab580653f6aaae55e78cab65bce1e2b27b05c
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-22 14:18:18 +0900

    [Done]Shared Digit

--- Desktop/JavaMasterClass/CodingExercises/src/SharedDigit.java
+++ Desktop/JavaMasterClass/CodingExercises/src/SharedDigit.java
@@ -1,5 +1,21 @@
 public class SharedDigit {
     public static void main(String[] args) {
 
+        System.out.println(hasSharedDigit(29, 99));
+
+    }
+    public static boolean hasSharedDigit(int x, int y){
+        if (x<10 || x>=100 || y<10 || y>=100) {
+            return false;
+        }
+
+        for (int i = x; i > 0; i /= 10) {
+            for (int j = y; j > 0; j /= 10) {
+                if(i%10==j%10){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
