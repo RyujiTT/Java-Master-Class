bd0db4d22dd8fd5eb6d1e0098790dc11d199005d
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-22 14:04:28 +0900

    [Done]Even Digit Sum

--- Desktop/JavaMasterClass/CodingExercises/src/EvenDigitSum.java
+++ Desktop/JavaMasterClass/CodingExercises/src/EvenDigitSum.java
@@ -1,5 +1,20 @@
 public class EvenDigitSum {
     public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(23546851));
 
+    }
+    public static int getEvenDigitSum(int number){
+        if (number < 0){
+            return -1;
+        }
+
+        int sum = 0;
+        int remainder = 0;
+        while (number > 0){
+            remainder = number % 10;
+            sum += (remainder % 2 == 0) ? remainder:0;
+            number /= 10;
+        }
+        return sum;
     }
 }
