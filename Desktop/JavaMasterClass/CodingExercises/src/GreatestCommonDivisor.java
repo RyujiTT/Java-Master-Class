b91c183e655e49a40988c10a9404068d86d44bfb
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-23 16:05:36 +0900

    [Done]Greatest Common Divisor

--- Desktop/JavaMasterClass/CodingExercises/src/GreatestCommonDivisor.java
+++ Desktop/JavaMasterClass/CodingExercises/src/GreatestCommonDivisor.java
@@ -1,2 +1,21 @@
 public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+
+        System.out.println(getGreatestCommonDivisor(294,70));
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+        while (first != second) {
+            if (first > second)
+                first = first - second;
+            else {
+                second = second - first;
+            }
+        }
+        return second;
+    }
 }
