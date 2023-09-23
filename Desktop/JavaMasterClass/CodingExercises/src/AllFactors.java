0fb8c2771439b5f558d88736fc16984905409930
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-23 15:54:35 +0900

    [Done]All Factors

--- Desktop/JavaMasterClass/CodingExercises/src/AllFactors.java
+++ Desktop/JavaMasterClass/CodingExercises/src/AllFactors.java
@@ -1,2 +1,16 @@
 public class AllFactors {
+
+    public static void main(String[] args) {
+        printFactors(17);
+    }
+    public static void printFactors(int x){
+        if(x < 1){
+            System.out.println("Invalid Value");
+        }
+        for(int i = 1; i<=x ;i++){
+            if(x % i == 0){
+                System.out.println(i);
+            }
+        }
+    }
 }
