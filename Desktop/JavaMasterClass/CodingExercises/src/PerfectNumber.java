df485e46ce581d2ec75457b0be2680bf24e2846b
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-24 16:37:23 +0900

    [Done]Perfect Number

--- Desktop/JavaMasterClass/CodingExercises/src/PerfectNumber.java
+++ Desktop/JavaMasterClass/CodingExercises/src/PerfectNumber.java
@@ -1,2 +1,16 @@
 public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(424));
+    }
+
+    public static boolean isPerfectNumber (int n) {
+        if(n < 1) return false;
+        int sum = 0;
+        double k = Math.sqrt(n);
+        for(int i = 1; i <= k; i++) {
+            if(n % i == 0) sum += i;
+            if(i != (n / i) && n % (n / i) == 0 && (n / i) != n) sum += n / i;
+        }
+        return sum == n;
+    }
 }
