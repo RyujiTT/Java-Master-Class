e9ffb12ff7e435e64718c3861ccb70582d4c2697
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-26 19:50:38 +0900

    [Done]Reading User Input

--- Desktop/JavaMasterClass/ReadingUserInput/src/Main.java
+++ Desktop/JavaMasterClass/ReadingUserInput/src/Main.java
@@ -3,5 +3,33 @@
 public class Main {
     public static void main(String[] args) {
 
+        int currentYear = 2023;
+
+        System.out.println(getInputFromConsole(currentYear));
+        System.out.println(getInputFromScanner(currentYear));
+//        String userDateOfBirth = "2000";
+//
+//        int dateOfBirth = Integer.parseInt(userDateOfBirth);
+//
+//        System.out.println("Age = " + (currentYear - dateOfBirth));
+//
+//        String usersAgeWithPartialYear = "22.5";
+//        double ageWithPartialYear = Double.parseDouble(usersAgeWithPartialYear);
+//        System.out.println("The user says he's " + ageWithPartialYear);
+    }
+
+    public static String getInputFromConsole(int currentYear){
+
+        String name = System.console().readLine("Hi, what's your Name?");
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+
+        String dateOfBirth = System.console().readLine("What year were you born?");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
+    }
+
+    public static String getInputFromScanner(int currentYear){
+        return "";
     }
 }
