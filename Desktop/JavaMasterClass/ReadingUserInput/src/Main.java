3353be59465c44b6fd62dcc35e0a5395dc2ed9df
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-27 22:03:22 +0900

    [Done]Introduction to Scanner

--- Desktop/JavaMasterClass/ReadingUserInput/src/Main.java
+++ Desktop/JavaMasterClass/ReadingUserInput/src/Main.java
@@ -1,11 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
         int currentYear = 2023;
 
-        System.out.println(getInputFromConsole(currentYear));
+//        System.out.println(getInputFromConsole(currentYear));
         System.out.println(getInputFromScanner(currentYear));
 //        String userDateOfBirth = "2000";
 //
@@ -18,18 +17,30 @@ public class Main {
 //        System.out.println("The user says he's " + ageWithPartialYear);
     }
 
-    public static String getInputFromConsole(int currentYear){
+//    public static String getInputFromConsole(int currentYear){
+//
+//        String name = System.console().readLine("Hi, what's your Name?");
+//        System.out.println("Hi " + name + ", Thanks for taking the course!");
+//
+//        String dateOfBirth = System.console().readLine("What year were you born?");
+//        int age = currentYear - Integer.parseInt(dateOfBirth);
+//
+//        return "So you are " + age + " years old";
+//    }
 
-        String name = System.console().readLine("Hi, what's your Name?");
+    public static String getInputFromScanner(int currentYear){
+
+        Scanner scanner = new Scanner(System.in);
+//        String name = System.console().readLine("Hi, what's your Name?");
+        System.out.println("Hi, what's your Name?");
+        String name = scanner.nextLine();
         System.out.println("Hi " + name + ", Thanks for taking the course!");
 
-        String dateOfBirth = System.console().readLine("What year were you born?");
+//        String dateOfBirth = System.console().readLine("What year were you born?");
+        System.out.println("What year were you born?");
+        String dateOfBirth = scanner.nextLine();
         int age = currentYear - Integer.parseInt(dateOfBirth);
 
         return "So you are " + age + " years old";
-    }
-
-    public static String getInputFromScanner(int currentYear){
-        return "";
     }
 }
