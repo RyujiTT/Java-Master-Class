2f00eccdd5865fdd47adebc8efff21497d68cb4b
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-27 22:29:05 +0900

    [Done]Diagonal Star

--- Desktop/JavaMasterClass/CodingExercises/src/DiagnalStar.java
+++ Desktop/JavaMasterClass/CodingExercises/src/DiagnalStar.java
@@ -1,5 +1,31 @@
 public class DiagnalStar {
     public static void main(String[] args) {
+        printSquareStar(25);
+    }
 
+    public static void printSquareStar(int number) {
+
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int row = 1; row <= number; row++) {
+
+            for (int col = 1; col <= number; col++) {
+
+                if (row == 1 || row == number ||
+                        col == 1 || col == number ||
+                        row == col || col == number - row + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+                if (col == number) {
+                    System.out.println();
+                }
+            }
+        }
     }
 }
