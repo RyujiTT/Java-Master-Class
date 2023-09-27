37c5ae3615198ad6d276484d86ba151673b51b71
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-27 22:19:45 +0900

    [Done]Largest Prime

--- Desktop/JavaMasterClass/CodingExercises/src/LargestPrime.java
+++ Desktop/JavaMasterClass/CodingExercises/src/LargestPrime.java
@@ -1,7 +1,23 @@
 public class LargestPrime {
     public static void main(String[] args) {
 
+        System.out.println(getLargestPrime(345));
+
     }
 
+    public static int getLargestPrime(int number){
+        if (number < 2) {
+            return -1;
+        }
 
+        int i = 2;
+        while(i <= number/2) {
+            if (number % i == 0) {
+                number /= i;
+                continue; // if divisor found, skip the i++
+            }
+            i++; // increment when divisor isn't found
+        }
+        return number;
+    }
 }
