9695d39930f3407f364dd946a7c2c89b45f3592c
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-27 22:11:59 +0900

    [Done]Flour Pack Problem

--- Desktop/JavaMasterClass/CodingExercises/src/FlourPackProblem.java
+++ Desktop/JavaMasterClass/CodingExercises/src/FlourPackProblem.java
@@ -1,2 +1,13 @@
 public class FlourPackProblem {
+    public static void main(String[] args) {
+
+        System.out.println(canPack(4, 5, 25));
+
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int Goal){
+        if (bigCount<0||smallCount<0||Goal<0) return false;
+        boolean rest = Goal%5<=smallCount;
+        return (bigCount*5+smallCount>=Goal)? rest : false;
+    }
 }
