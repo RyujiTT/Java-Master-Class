8bd9df4fbcb8b21417e730c705fd313f97eba9ef
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-28 11:34:31 +0900

    [Add]Person

--- Desktop/JavaMasterClass/CodingExercises/src/Main.java
+++ Desktop/JavaMasterClass/CodingExercises/src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        SimpleCalculator calculator = new SimpleCalculator();
-        calculator.setFirstNumber(49.4);
-        calculator.setSecondNumber(8.5);
-
-        System.out.println(calculator.getAdditionResult());
-        calculator.getAdditionResult();
-        calculator.getSubtractionResult();
-        calculator.getMultiplicationResult();
-        calculator.getDivisionResult();
-
     }
 }
