31fe86f54e01bb7aeed68e8a40089492a7438dc2
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-28 10:01:34 +0900

    [Done]Job Paint

--- Desktop/JavaMasterClass/CodingExercises/src/JobPaint.java
+++ Desktop/JavaMasterClass/CodingExercises/src/JobPaint.java
@@ -1,2 +1,23 @@
 public class JobPaint {
+
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(4,1.5,13,4));
+    }
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if(width <= 0 || height <=0 || areaPerBucket <= 0 || extraBuckets <0) {
+            return -1;
+        }
+        double area = width * height;
+        double firstEstimatedArea = areaPerBucket * extraBuckets;
+        double buckets = Math.ceil((area - firstEstimatedArea) / areaPerBucket);
+        return (int) buckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        return getBucketCount(width,height,areaPerBucket,0);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        return getBucketCount(area,1,areaPerBucket,0);
+    }
 }
