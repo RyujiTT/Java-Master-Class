e26c07933ecedff549aac4527fdb4fa7a75590e4
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-29 12:21:18 +0900

    [Done]Record Project part2

--- Desktop/JavaMasterClass/RecordProject/src/Main.java
+++ Desktop/JavaMasterClass/RecordProject/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         for(int i = 1; i <= 5; i++){
-            Student s = new Student("s92335" + i,
+            LPAStudent s = new LPAStudent("s92335" + i,
                     switch (i){
                 case 1 -> "Mary";
                 case 2 -> "Carol";
@@ -14,5 +14,18 @@ public class Main {
                     "Java Masterclass");
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("s923356", "Ann", "09/29/2000", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("s923357", "James", "09/29/2000", "Java Maserclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList((pojoStudent.getClassList() + ", Java OCP Exam 892"));
+
+        System.out.println(pojoStudent.getName() + " is taking " +
+                pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " +
+                recordStudent.classList());
     }
 }
