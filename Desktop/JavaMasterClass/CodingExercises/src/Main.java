0cd0ed4a392c8a41e7e9c600d8ffd869447f6c84
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-29 12:42:29 +0900

    [Done]Point

--- Desktop/JavaMasterClass/CodingExercises/src/Main.java
+++ Desktop/JavaMasterClass/CodingExercises/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        Point point = new Point(34,5);
+
+        System.out.println(point.distance(234,35));
+
     }
 }
--- Desktop/JavaMasterClass/CodingExercises/src/Point.java
+++ Desktop/JavaMasterClass/CodingExercises/src/Point.java
@@ -1,2 +1,42 @@
 public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(){
+        return distance(0, 0);
+    }
+
+    public double distance(int xB, int yB){
+        return Math.sqrt((xB - x) * (xB - x) + (yB - y) * (yB -y));
+    }
+
+    public double distance(Point otherPoint){
+        return distance(otherPoint.getX(), otherPoint.getY());
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
