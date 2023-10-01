ea7e13e930c95ee38e844715ecb85429f28bf9e8
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-10-01 21:41:48 +0900

    [Done]Inheritance part1

--- Desktop/JavaMasterClass/Inheritance/src/Dog.java
+++ Desktop/JavaMasterClass/Inheritance/src/Dog.java
@@ -1,2 +1,6 @@
-public class Dog {
+public class Dog extends Animal{
+
+    public Dog(){
+        super("Mutt", "Big", 50);
+    }
 }
--- Desktop/JavaMasterClass/Inheritance/src/Main.java
+++ Desktop/JavaMasterClass/Inheritance/src/Main.java
@@ -2,6 +2,17 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
 
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed){
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
     }
 }
